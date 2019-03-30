05ad6f40ee17a5567c854294af3c0c898e584f9a
heidygerbi <heidyjoanagr@gmail.com>
2019-03-30 16:52:12 -0500

    contenido de los cursos 2

--- src/UI/test.js
+++ src/UI/test.js
@@ -4,23 +4,23 @@ export default () => {
   <h1>TEST HTML5</h1>
   <h2>1) Según lo leido anteriormente, ¿qué afirmaciones son correctas?</h2>
   <form class="resp">
-      <label><input type="checkbox" id="answer1-1">La estructura doctype</label>
-      <label><input type="checkbox" id="answer1-2">HTML5 es un lenguaje de programación</label>
-      <label><input type="checkbox" id="answer1-3">HTML5 es la última versión de HTML</label>
-      <label><input type="checkbox" id="answer1-4">HTML surge en el año 2010</label>
+      <label><input type="checkbox" id="answer1-1" name="answer1">La estructura doctype</label><br>
+      <label><input type="checkbox" id="answer1-2" name="answer1">HTML5 es un lenguaje de programación</label><br>
+      <label><input type="checkbox" id="answer1-3" name="answer1">HTML5 es la última versión de HTML</label><br>
+      <label><input type="checkbox" id="answer1-4" name="answer1">HTML surge en el año 2010</label><br>
   </form>
   <h2>2) HTML5, ¿es un lenguaje de programación?</h2>
   <form class="resp">
-      <label><input type="radio" id="answer2-1">Verdadero</label>
-      <label><input type="radio" id="answer2-2">Falso</label>
+      <label><input type="radio" id="answer2-1" name="answer2">Verdadero</label><br>
+      <label><input type="radio" id="answer2-2" name="answer2">Falso</label><br>
 
   </form>
   <h2>3) Según lo leido anteriormente, ¿qué afirmación son correcta?</h2>
   <form class="resp">
-      <label><input type="radio" id="answer3-1">Nav es una etiqueta HTML</label>
-      <label><input type="radio" id="answer3-2">JS y HTML sirven para lo mismo</label>
-      <label><input type="radio" id="answer3-3">CSS3 sustituye a HTML5</label>
-      <label><input type="radio" id="answer3-4">la etiqueta center se utiliza en HTML5</label>
+      <label><input type="radio" id="answer3-1" name="answer3">Nav es una etiqueta HTML</label><br>
+      <label><input type="radio" id="answer3-2" name="answer3">JS y HTML sirven para lo mismo</label><br>
+      <label><input type="radio" id="answer3-3" name="answer3">CSS3 sustituye a HTML5</label><br>
+      <label><input type="radio" id="answer3-4" name="answer3">la etiqueta center se utiliza en HTML5</label><br>
   </form>
   <a id="submit-test" href="resulEvaluationHTML.html">Enviar test</a>`;
   sectionElem.setAttribute('id', 'content-html');
@@ -31,6 +31,7 @@ export default () => {
     // window.location.hash = '#/test';
     let resp = document.getElementsByClassName('resp');
     console.log('resp');
+    // resp debe ser un array de respuestas
   });
   return sectionElem;
 };
